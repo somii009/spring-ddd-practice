88ead54d418e27577d89d232d91979a3598487c4
Somi <rlathal009@gmail.com>
2022-12-25 01:40:25 +0900

    feat : Exception handling and errorcode

---
+++ src/main/java/com/practice/order/common/exception/EntityNotFoundException.java
@@ -0,0 +1,14 @@
+package com.practice.order.common.exception;
+
+import com.practice.order.common.response.ErrorCode;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
---
+++ src/main/java/com/practice/order/common/exception/IllegalStatusException.java
@@ -0,0 +1,14 @@
+package com.practice.order.common.exception;
+
+import com.practice.order.common.response.ErrorCode;
+
+public class IllegalStatusException extends BaseException {
+
+    public IllegalStatusException() {
+        super(ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+
+    public IllegalStatusException(String message) {
+        super(message, ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+}
