427c8013e4ceaadc6f5acc1d02d7a70895f62462
Somi <rlathal009@gmail.com>
2022-12-25 01:39:43 +0900

    feat : create PartnerInfo class that returns partner info

---
+++ src/main/java/com/practice/order/domain/partner/PartnerInfo.java
@@ -0,0 +1,24 @@
+package com.practice.order.domain.partner;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PartnerInfo {
+    private Long id;
+    private String partnerToken;
+    private String partnerName;
+    private String businessNo;
+    private String email;
+    private Partner.Status status;
+
+    public PartnerInfo(Partner partner) {
+        this.id = partner.getId();
+        this.partnerToken = partner.getPartnerToken();
+        this.partnerName = partner.getPartnerName();
+        this.businessNo = partner.getBusinessNo();
+        this.email = partner.getEmail();
+        this.status = partner.getStatus();
+    }
+
+}
