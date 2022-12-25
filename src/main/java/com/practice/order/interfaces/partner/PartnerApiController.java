8f00665a1c6cf1451db857df1b46e00ebc9c26d1
Somi <rlathal009@gmail.com>
2022-12-26 01:25:09 +0900

    feat : add PartnerApiController

---
+++ src/main/java/com/practice/order/interfaces/partner/PartnerApiController.java
@@ -0,0 +1,43 @@
+package com.practice.order.interfaces.partner;
+
+import com.practice.order.application.partner.PartnerFacade;
+import com.practice.order.common.response.CommonResponse;
+import com.practice.order.domain.partner.PartnerCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/partners")
+public class PartnerApiController {
+    private final PartnerFacade partnerFacade;
+
+    @PostMapping
+    public CommonResponse registerPartner(PartnerDto.RegisterRequest request) {
+        // 1. 외부에서 전달된 파라미터 (dto) -> Command, Criteria로 convert
+        // 2. facade 호출 .. PartnerInfo
+        // 3. PartnerInfo -> CommonResponse convert AND return
+
+        var command = PartnerCommand.builder()
+                .partnerName(request.getPartnerName())
+                .businessNo(request.getBusinessNo())
+                .email(request.getEmail())
+                .build();
+
+        var partnerInfo = partnerFacade.registerPartner(command);
+
+        var response = PartnerDto.RegisterResponse.builder()
+                .partnerToken()
+                .partnerName()
+                .businessNo()
+                .email()
+                .status()
+                .build();
+
+        return CommonResponse.success(response);
+    }
+}
