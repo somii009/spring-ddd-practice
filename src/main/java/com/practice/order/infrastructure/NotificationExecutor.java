656be7b7f4777e7dd729da7bffd6d6dbfa4ba737
Somi <rlathal009@gmail.com>
2022-12-26 01:14:08 +0900

    feat : create NotificationExecutor

---
+++ src/main/java/com/practice/order/infrastructure/NotificationExecutor.java
@@ -0,0 +1,24 @@
+package com.practice.order.infrastructure;
+
+import com.practice.order.domain.notification.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class NotificationExecutor implements NotificationService {
+    @Override
+    public void sendEmail(String email, String title, String description) {
+        log.info("sendEmail");
+    }
+
+    @Override
+    public void sendKakao(String phoneNo, String description) {
+        log.info("sendKakao");
+    }
+
+    @Override
+    public void sendSms(String phoneNo, String description) {
+        log.info("sendSms");
+    }
+}
