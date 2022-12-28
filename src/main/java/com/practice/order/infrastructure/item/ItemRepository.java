93ea6c6781eb9d456c925b588ec724f309d3c9b4
Somi <rlathal009@gmail.com>
2022-12-28 23:57:38 +0900

    feat : create ItemInfo

---
+++ src/main/java/com/practice/order/infrastructure/item/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.practice.order.infrastructure.item;
+
+import com.practice.order.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findByItemToken(String itemToken);
+}
