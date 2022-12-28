e410c0e5f16525a6cab80e9ca67ae16be30c5d64
Somi <rlathal009@gmail.com>
2022-12-28 23:56:05 +0900

    feat : create ItemService and ItemServiceImpl

---
+++ src/main/java/com/practice/order/domain/item/ItemService.java
@@ -0,0 +1,8 @@
+package com.practice.order.domain.item;
+
+public interface ItemService {
+    String registerItem(ItemCommand.RegisterItemRequest request, String partnerToken);
+    void changeOnSale(String itemToken);
+    void changeEndOfSale(String itemToken);
+    ItemInfo.Main retrieveItemInfo(String itemToken);
+}
