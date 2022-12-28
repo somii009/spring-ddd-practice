09c2c0c9a93f81d1b70873e33d8bc31c175261c2
Somi <rlathal009@gmail.com>
2022-12-28 23:56:34 +0900

    feat : create ItemReader

---
+++ src/main/java/com/practice/order/domain/item/ItemReader.java
@@ -0,0 +1,8 @@
+package com.practice.order.domain.item;
+
+import java.util.List;
+
+public interface ItemReader {
+    Item getItemBy(String itemToken);
+    List<ItemInfo.ItemOptionGroupInfo> getItemOptionSeries(Item item);
+}
